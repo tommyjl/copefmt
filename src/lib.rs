a7632e6b1504f2c5ec1bda52e8f2d32fc1503311
Tommy Joe Lund <tomlun92@gmail.com>
2022-11-07 23:58:13 +0100

    Add configurable handling for different parens

--- src/lib.rs
+++ src/lib.rs
@@ -4,9 +4,15 @@ pub struct Formatter<R: Read, W: Write> {
     reader: BufReader<R>,
     writer: BufWriter<W>,
     current: u8,
+    previous: u8,
     newline: String,
     indent: String,
-    indent_level: u32,
+    levels: Vec<Level>,
+}
+
+#[derive(Clone)]
+struct Level {
+    break_newline: bool,
 }
 
 impl<R: Read, W: Write> Formatter<R, W> {
@@ -15,9 +21,10 @@ impl<R: Read, W: Write> Formatter<R, W> {
             reader: BufReader::new(reader),
             writer: BufWriter::new(writer),
             current: 0,
+            previous: 0,
             newline: "\n".to_string(),
             indent: "    ".to_string(),
-            indent_level: 0,
+            levels: Vec::with_capacity(10),
         }
     }
 
@@ -32,30 +39,75 @@ impl<R: Read, W: Write> Formatter<R, W> {
 
 // Private methods
 impl<R: Read, W: Write> Formatter<R, W> {
+    fn push_level(&mut self, level: Level) {
+        self.levels.push(level);
+    }
+
+    fn pop_level(&mut self) {
+        if !self.levels.is_empty() {
+            self.levels.pop();
+        }
+    }
+
+    fn current_level(&self) -> Level {
+        self.levels.last().cloned().unwrap_or(Level {
+            break_newline: false,
+        })
+    }
+
+    fn indent_level(&self) -> i32 {
+        self.levels.len() as _
+    }
+
     fn read_next(&mut self) -> bool {
+        self.previous = self.current;
         let buf = std::slice::from_mut(&mut self.current);
         self.reader.read_exact(buf).is_ok()
     }
 
     fn update(&mut self) {
-        self.update_indent();
+        self.update_level();
     }
 
-    fn update_indent(&mut self) {
+    fn update_level(&mut self) {
+        match self.previous {
+            b'}' | b')' if self.indent_level() > 0 => self.pop_level(),
+            _ => {}
+        };
         match self.current {
-            b'{' | b'(' => self.indent_level += 1,
-            b'}' | b')' if self.indent_level > 0 => self.indent_level -= 1,
+            b'{' => self.push_level(Level {
+                break_newline: true,
+            }),
+            b'(' => self.push_level(Level {
+                break_newline: false,
+            }),
             _ => {}
         };
     }
 
     fn write(&mut self) -> std::io::Result<()> {
-        self.write_newline_before()?;
+        match self.previous {
+            b'{' | b'(' | b',' => {
+                if self.current_level().break_newline {
+                    self.write_newline()?;
+                    self.write_indent(self.indent_level())?;
+                }
+            }
+            _ => {}
+        };
 
-        let buf = std::slice::from_ref(&self.current);
-        self.writer.write_all(buf).unwrap();
+        match self.current {
+            b'}' | b')' => {
+                if self.current_level().break_newline {
+                    self.write_newline()?;
+                    self.write_indent(self.indent_level() - 1)?;
+                }
+            }
+            _ => {}
+        };
 
-        self.write_newline_after()?;
+        let current = std::slice::from_ref(&self.current);
+        self.writer.write_all(current).unwrap();
 
         Ok(())
     }
@@ -64,31 +116,9 @@ impl<R: Read, W: Write> Formatter<R, W> {
         self.writer.write_all(self.newline.as_bytes())
     }
 
-    fn write_newline_before(&mut self) -> std::io::Result<()> {
-        match self.current {
-            b'}' | b')' => {
-                self.write_newline()?;
-                self.write_indent()?;
-                Ok(())
-            }
-            _ => Ok(()),
-        }
-    }
-
-    fn write_newline_after(&mut self) -> std::io::Result<()> {
-        match self.current {
-            b'{' | b'(' | b',' => {
-                self.write_newline()?;
-                self.write_indent()?;
-                Ok(())
-            }
-            _ => Ok(()),
-        }
-    }
-
-    fn write_indent(&mut self) -> std::io::Result<()> {
+    fn write_indent(&mut self, n: i32) -> std::io::Result<()> {
         let buf = self.indent.as_bytes();
-        for _ in 0..self.indent_level {
+        for _ in 0..n {
             self.writer.write_all(buf)?;
         }
         Ok(())
